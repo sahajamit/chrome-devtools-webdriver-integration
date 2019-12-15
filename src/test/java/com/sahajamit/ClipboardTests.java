f9e3cf4ae36f848efefb879ac778490bffd00967
Amit Rawat <sahajamit@gmail.com>
2019-12-15 21:31:57 +0800

    basic code to read local clipboard content

---
+++ src/test/java/com/sahajamit/ClipboardTests.java
@@ -0,0 +1,56 @@
+package com.sahajamit;
+
+import com.sahajamit.utils.UINotificationService;
+import com.sahajamit.utils.UIUtils;
+import com.sahajamit.utils.Utils;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+
+import java.awt.*;
+import java.awt.datatransfer.DataFlavor;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class ClipboardTests {
+
+    private WebDriver driver;
+    private String wsURL;    private Utils utils;
+    private UIUtils uiUtils;
+    private ChromeDriverService chromeDriverService;
+
+    @Before
+    public void beforeTest(){
+        this.utils = Utils.getInstance();
+        this.uiUtils = UIUtils.getInstance();
+    }
+
+    @After
+    public void afterTest(){
+        utils.stopChrome();
+        if(!Objects.isNull(chromeDriverService))
+            chromeDriverService.stop();
+    }
+    @Test
+    public void doClipboardReadTest() throws Exception {
+        String text = "Hello World!";
+        driver = utils.launchBrowser();
+        driver.navigate().to("https://googlechrome.github.io/samples/async-clipboard/");
+
+        driver.findElement(By.cssSelector("textarea#out")).sendKeys(text);
+        utils.waitFor(1);
+        uiUtils.takeScreenShot();
+        driver.findElement(By.cssSelector("button#copy")).click();
+        utils.waitFor(1);
+        String localClipboardData = (String) Toolkit.getDefaultToolkit()
+                .getSystemClipboard().getData(DataFlavor.stringFlavor);
+        Assert.assertEquals(text,localClipboardData);
+
+    }
+
+}
